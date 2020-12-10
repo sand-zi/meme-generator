9c0887db46d7b985dce3b0d1b249eca35149cfd5
Sandra Zufman <zinkeen@gmail.com>
2020-12-10 16:53:39 +0200

    project as of 17 thursday

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,87 @@
 'use strict'
+
+
+var gCanvas;
+var gCtx;
+var gContureColor = 'black';
+var gBgcColor = 'white';
+var gChosenImgId
+
+function init() {
+    gCanvas = document.getElementById('my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderGallery();
+}
+
+function renderGallery() {
+    var images = getImagesForDisplay();
+    var strHtmls = images.map(function (img) {
+        return `
+            <img class="img-top" src=${img.url} onclick="onOpenCanvas(${img.id})">
+        `
+    })
+    document.querySelector('.gallery-container').innerHTML = strHtmls.join('')
+}
+
+function onOpenCanvas(imgIdx) {
+    gChosenImgId = +imgIdx;
+    document.querySelector('.gallery-container').classList.add('hidden')
+    document.querySelector('.editor-container').classList.remove('hidden')
+    document.querySelector('.editor-container').classList.add('flex')
+    renderCanvas();
+}
+
+function renderCanvas() {
+    var meme = getMeme(gChosenImgId);
+    var img = new Image();
+    img.src = `img/${meme.selectedImgId}.jpg`;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+        meme.lines.forEach(line => {
+            drawText(line.txt, line.size, line.align, line.x, line.y)
+        }
+        )
+    }
+}
+
+
+function drawText(text, fontSize, align, x, y) {
+    gCtx.strokeStyle = gContureColor;
+    gCtx.fillStyle = gBgcColor;
+    gCtx.font = `${fontSize}px Impact`;
+    gCtx.textAlign = `${align}`;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+
+function onUpdateMemeText(el) {
+    updateMemeText(el.value);
+    renderCanvas();
+}
+
+
+function onChangeFontSize(diff) {
+    changeMemeFontSize(diff)
+    renderCanvas()
+}
+
+
+function onMoveLines(ev) {
+    if (ev.offsetY >= ev.target.height / 2) {
+        moveMemeText(10, gCanvas.height)
+    } else moveMemeText(-10, gCanvas.height)
+    renderCanvas()
+}
+
+
+function onAlign(el) {
+    updateAlign(el.name);
+    renderCanvas();
+}
+
+
+function onAddLine() {
+    addLine();
+    renderCanvas();
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -13,35 +13,101 @@ var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['man', 'president', 'mood'] }
 { id: 10, url: 'img/10.jpg', keywords: ['laugh', 'man', 'president'] },
 ];
 
+var gMeme
 
-
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
-}
-
+createMeme()
 
 function createMeme() {
-    return {
-
+    gMeme = {
         selectedImgId: 5,
         selectedLineIdx: 0,
         lines: [
             {
-                txt: 'I never eat Falafel',
-                size: 20,
-                align: 'left',
-                color: 'red'
+                txt: 'I am working',
+                size: 30,
+                align: 'center',
+                color: 'red',
+                x: 250,
+                y: 50,
             }
         ]
-
     }
+}
+
+function getMeme(imgId) {
+    gMeme.selectedImgId = imgId;
+    var meme = gMeme;
+    return meme;
+}
+
+function updateMemeText(text) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = text;
+}
+
+function updateAlign(align) {
+    switch (align) {
+        case 'right':
+            gMeme.lines[gMeme.selectedLineIdx].x = 350;
+            break;
+        case 'center':
+            gMeme.lines[gMeme.selectedLineIdx].x = 250;
+            break;
+        case 'left':
+            gMeme.lines[gMeme.selectedLineIdx].x = 100;
+            break;
+    }
+}
+
+function changeMemeFontSize(diff) {
+    if (gMeme.lines[gMeme.selectedLineIdx].size > 62) {
+        gMeme.lines[gMeme.selectedLineIdx].size = 62
+    }
+    if (gMeme.lines[gMeme.selectedLineIdx].size < 12) {
+        gMeme.lines[gMeme.selectedLineIdx].size = 12
+    }
+
+    gMeme.lines[gMeme.selectedLineIdx].size += diff
+}
+
+function addLine() {
+    var newLine = createNewLine()
+    gMeme.lines.push(newLine)
+};
+
+function createNewLine() {
+    return {
+        txt: 'Enter Your text Here',
+        size: 30,
+        align: 'center',
+        color: 'red',
+        x: 250,
+        y: getVertPosition(),
+    }
+}
+
+function getVertPosition() {
+    if (gMeme.lines.length === 1) {
+        return 420
+    }
+
+    if (gMeme.lines.length > 1) {
+        return 250
+    }
+}
+function moveMemeText(diff, maxHeight) {
+    if (gMeme.lines[gMeme.selectedLineIdx].y > 0.95 * maxHeight) {
+        gMeme.lines[gMeme.selectedLineIdx].y = 0.95 * maxHeight
+    }
+    if (gMeme.lines[gMeme.selectedLineIdx].y < 45) {
+        gMeme.lines[gMeme.selectedLineIdx].y = 50
+    }
+    gMeme.lines[gMeme.selectedLineIdx].y += diff
+}
+
+
+// Gallery
+
+function getImagesForDisplay() {
+    var images = gImgs;
+    return images;
 }
